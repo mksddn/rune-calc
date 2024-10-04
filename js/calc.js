276720553da91b7b1783b6011db4a97df68a5886
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-04 13:03:09 +0500

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -4,95 +4,97 @@ document.addEventListener("DOMContentLoaded", function () {
 		{
 			group: 1,
 			atts: [
-				{ rus: "Феху", eng: "Fehu", img: "img/runes/fehu.png" },
-				{ rus: "Хагалаз", eng: "Hagalaz", img: "img/runes/hagalaz.png" },
-				{ rus: "Тейваз", eng: "Teiwaz", img: "img/runes/teiwaz.png" }
+				{ rus: "Феху", eng: "Fehu", img: "/img/runes/fehu.png" },
+				{ rus: "Хагалаз", eng: "Hagalaz", img: "/img/runes/hagalaz.png" },
+				{ rus: "Тейваз", eng: "Teiwaz", img: "/img/runes/teiwaz.png" }
 			]
 		},
 		{
 			group: 2,
 			atts: [
-				{ rus: "Уруз", eng: "Uruz", img: "img/runes/uruz.png" },
-				{ rus: "Наутиз", eng: "Nauthiz", img: "img/runes/nauthiz.png" },
-				{ rus: "Беркана", eng: "Berkana", img: "img/runes/berkana.png" }
+				{ rus: "Уруз", eng: "Uruz", img: "/img/runes/uruz.png" },
+				{ rus: "Наутиз", eng: "Nauthiz", img: "/img/runes/nauthiz.png" },
+				{ rus: "Беркана", eng: "Berkana", img: "/img/runes/berkana.png" }
 			]
 		},
 		{
 			group: 3,
 			atts: [
-				{ rus: "Турисаз", eng: "Thurisaz", img: "img/runes/thurisaz.png" },
-				{ rus: "Иса", eng: "Isa", img: "img/runes/isa.png" },
-				{ rus: "Эваз", eng: "Ehwaz", img: "img/runes/ehwaz.png" }
+				{ rus: "Турисаз", eng: "Thurisaz", img: "/img/runes/thurisaz.png" },
+				{ rus: "Иса", eng: "Isa", img: "/img/runes/isa.png" },
+				{ rus: "Эваз", eng: "Ehwaz", img: "/img/runes/ehwaz.png" }
 			]
 		},
 		{
 			group: 4,
 			atts: [
-				{ rus: "Ансуз", eng: "Ansuz", img: "img/runes/ansuz.png" },
-				{ rus: "Йера", eng: "Jera", img: "img/runes/jera.png" },
-				{ rus: "Манназ", eng: "Mannaz", img: "img/runes/mannaz.png" }
+				{ rus: "Ансуз", eng: "Ansuz", img: "/img/runes/ansuz.png" },
+				{ rus: "Йера", eng: "Jera", img: "/img/runes/jera.png" },
+				{ rus: "Манназ", eng: "Mannaz", img: "/img/runes/mannaz.png" }
 			]
 		},
 		{
 			group: 5,
 			atts: [
-				{ rus: "Райдо", eng: "Raido", img: "img/runes/raido.png" },
-				{ rus: "Эйваз", eng: "Eihwaz", img: "img/runes/eihwaz.png" },
-				{ rus: "Лагуз", eng: "Laguz", img: "img/runes/laguz.png" }
+				{ rus: "Райдо", eng: "Raido", img: "/img/runes/raido.png" },
+				{ rus: "Эйваз", eng: "Eihwaz", img: "/img/runes/eihwaz.png" },
+				{ rus: "Лагуз", eng: "Laguz", img: "/img/runes/laguz.png" }
 			]
 		},
 		{
 			group: 6,
 			atts: [
-				{ rus: "Кеназ", eng: "Kenaz", img: "img/runes/kenaz.png" },
-				{ rus: "Перт", eng: "Perth", img: "img/runes/perth.png" },
-				{ rus: "Ингуз", eng: "Inguz", img: "img/runes/inguz.png" }
+				{ rus: "Кеназ", eng: "Kenaz", img: "/img/runes/kenaz.png" },
+				{ rus: "Перт", eng: "Perth", img: "/img/runes/perth.png" },
+				{ rus: "Ингуз", eng: "Inguz", img: "/img/runes/inguz.png" }
 			]
 		},
 		{
 			group: 7,
 			atts: [
-				{ rus: "Гебо", eng: "Gebo", img: "img/runes/gebo.png" },
-				{ rus: "Альгиз", eng: "Algiz", img: "img/runes/algiz.png" },
-				{ rus: "Отала", eng: "Othala", img: "img/runes/othala.png" }
+				{ rus: "Гебо", eng: "Gebo", img: "/img/runes/gebo.png" },
+				{ rus: "Альгиз", eng: "Algiz", img: "/img/runes/algiz.png" },
+				{ rus: "Отала", eng: "Othala", img: "/img/runes/othala.png" }
 			]
 		},
 		{
 			group: 8,
 			atts: [
-				{ rus: "Вуньо", eng: "Wunjo", img: "img/runes/wunjo.png" },
-				{ rus: "Совило", eng: "Sowulo", img: "img/runes/sowulo.png" },
-				{ rus: "Дагаз", eng: "Dagaz", img: "img/runes/dagaz.png" }
+				{ rus: "Вуньо", eng: "Wunjo", img: "/img/runes/wunjo.png" },
+				{ rus: "Совило", eng: "Sowulo", img: "/img/runes/sowulo.png" },
+				{ rus: "Дагаз", eng: "Dagaz", img: "/img/runes/dagaz.png" }
 			]
 		},
 		{
 			group: 9,
 			atts: [
-				{ rus: "Пустая руна<br>Вирд", eng: "Wyrd", img: "img/runes/empty.png" },
-				{ rus: "Пустая руна<br>Вирд", eng: "Wyrd", img: "img/runes/empty.png" },
-				{ rus: "Пустая руна<br>Вирд", eng: "Wyrd", img: "img/runes/empty.png" }
+				{ rus: "Пустая руна<br>Вирд", eng: "Wyrd", img: "/img/runes/empty.png" },
+				{ rus: "Пустая руна<br>Вирд", eng: "Wyrd", img: "/img/runes/empty.png" },
+				{ rus: "Пустая руна<br>Вирд", eng: "Wyrd", img: "/img/runes/empty.png" }
 			]
 		}
 	];
 
+	const ORIGIN_URL= window.location.origin;
+
 	// Функция для получения руны по номеру группы и этта
 	function getRuneByGroupAndAtt(groupNumber, attNumber) {
-	const groupIndex = groupNumber - 1; // Индекс группы
-	const attIndex = attNumber - 1; // Индекс этта
-	if (runes[groupIndex] && runes[groupIndex].atts[attIndex]) {
-		return {
-			rune: runes[groupIndex].atts[attIndex], // Руна
-			group: groupNumber, // Номер группы
-			att: attNumber, // Номер этта
-		};
-	} else {
-		return null; // Если такой группы или этта нет, вернем null
-	}
+		const groupIndex = groupNumber - 1; // Индекс группы
+		const attIndex = attNumber - 1; // Индекс этта
+		if (runes[groupIndex] && runes[groupIndex].atts[attIndex]) {
+			return {
+				rune: runes[groupIndex].atts[attIndex], // Руна
+				group: groupNumber, // Номер группы
+				att: attNumber, // Номер этта
+			};
+		} else {
+			return null; // Если такой группы или этта нет, вернем null
+		}
 	}
 
 	const rcForm = document.querySelector("#runeCalcForm");
 
-	rcForm.addEventListener("submit", () => {
+	rcForm.addEventListener("submit", (event) => {
 	event.preventDefault();
 
 	const language = rcForm.querySelector("#rc_language").value;
@@ -285,13 +287,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	document.querySelector(
 		"#essenceRune"
-	).innerHTML = `<img src="${essenceRune.rune.img}" alt="${essenceRune.rune.rus}"><h5><em>${essenceRune.rune.rus} (${essenceRune.rune.eng})</em></h5><small>Руническая группа: ${essenceRune.group}<br>Атт: ${essenceRune.att}</small>`;
+	).innerHTML = `<img src="${ORIGIN_URL+essenceRune.rune.img}" alt="${essenceRune.rune.rus}"><h5><em>${essenceRune.rune.rus} (${essenceRune.rune.eng})</em></h5><small>Руническая группа: ${essenceRune.group}<br>Атт: ${essenceRune.att}</small>`;
 	document.querySelector(
 		"#personalityRune"
-	).innerHTML = `<img src="${personalityRune.rune.img}" alt="${personalityRune.rune.rus}"><h5><em>${personalityRune.rune.rus} (${personalityRune.rune.eng})</em></h5><small>Руническая группа: ${personalityRune.group}<br>Атт: ${personalityRune.att}</small>`;
+	).innerHTML = `<img src="${ORIGIN_URL+personalityRune.rune.img}" alt="${personalityRune.rune.rus}"><h5><em>${personalityRune.rune.rus} (${personalityRune.rune.eng})</em></h5><small>Руническая группа: ${personalityRune.group}<br>Атт: ${personalityRune.att}</small>`;
 	document.querySelector(
 		"#goldenRune"
-	).innerHTML = `<img src="${goldenRune.rune.img}" alt="${goldenRune.rune.rus}"><h5><em>${goldenRune.rune.rus} (${goldenRune.rune.eng})</em></h5><small>Руническая группа: ${goldenRune.group}<br>Атт: ${goldenRune.att}</small>`;
+	).innerHTML = `<img src="${ORIGIN_URL+goldenRune.rune.img}" alt="${goldenRune.rune.rus}"><h5><em>${goldenRune.rune.rus} (${goldenRune.rune.eng})</em></h5><small>Руническая группа: ${goldenRune.group}<br>Атт: ${goldenRune.att}</small>`;
 	document.querySelector("#runeCalcResult").style.display = "block";
 	document.querySelector("#runeCalcAbout").style.display = "none";
 	document
