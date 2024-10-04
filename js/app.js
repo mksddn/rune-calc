28397752ae3dfa16c2c00bf2df48a9e580c80e00
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-04 13:03:07 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // SCROLL
-  document.querySelectorAll('a[href^="#"]:not(.popup-link)').forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-      let hash = this.getAttribute("href").substring(1);
-      scroll(hash);
-    });
-  });
+	// SCROLL
+	document.querySelectorAll('a[href^="#"]:not(.popup-link)').forEach((link) => {
+		link.addEventListener("click", function (e) {
+			e.preventDefault();
+			let hash = this.getAttribute("href").substring(1);
+			scroll(hash);
+		});
+	});
 
-  function scroll(hash) {
-    const scrollTarget = document.getElementById(hash);
-    if (scrollTarget) {
-      // const topOffset = $nav ? $nav.offsetHeight : 0;
-      const topOffset = 0; // если не нужен отступ сверху
-      const elementPosition = scrollTarget.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - topOffset;
-      window.scrollBy({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
-    }
-  }
+	function scroll(hash) {
+		const scrollTarget = document.getElementById(hash);
+		if (scrollTarget) {
+			// const topOffset = $nav ? $nav.offsetHeight : 0;
+			const topOffset = 0; // если не нужен отступ сверху
+			const elementPosition = scrollTarget.getBoundingClientRect().top;
+			const offsetPosition = elementPosition - topOffset;
+			window.scrollBy({
+			top: offsetPosition,
+			behavior: "smooth",
+			});
+		}
+	}
 
-  // SPOILER
-  const accordions = document.querySelectorAll(".spoiler");
-  const openAccordion = (accordion) => {
-    const content = accordion.querySelector(".spoiler__content");
-    accordion.classList.add("spoiler__active");
-    content.style.maxHeight = content.scrollHeight + "px";
-  };
-  const closeAccordion = (accordion) => {
-    const content = accordion.querySelector(".spoiler__content");
-    accordion.classList.remove("spoiler__active");
-    content.style.maxHeight = null;
-  };
-  accordions.forEach((accordion) => {
-    const intro = accordion.querySelector(".spoiler__intro");
-    const content = accordion.querySelector(".spoiler__content");
-    intro.onclick = () => {
-      if (content.style.maxHeight) {
-        closeAccordion(accordion);
-      } else {
-        accordions.forEach((accordion) => closeAccordion(accordion));
-        openAccordion(accordion);
-      }
-    };
-  });
+	// SPOILER
+	const accordions = document.querySelectorAll(".spoiler");
+	const openAccordion = (accordion) => {
+		const content = accordion.querySelector(".spoiler__content");
+		accordion.classList.add("spoiler__active");
+		content.style.maxHeight = content.scrollHeight + "px";
+	};
+	const closeAccordion = (accordion) => {
+		const content = accordion.querySelector(".spoiler__content");
+		accordion.classList.remove("spoiler__active");
+		content.style.maxHeight = null;
+	};
+	accordions.forEach((accordion) => {
+		const intro = accordion.querySelector(".spoiler__intro");
+		const content = accordion.querySelector(".spoiler__content");
+		intro.onclick = () => {
+			if (content.style.maxHeight) {
+			closeAccordion(accordion);
+			} else {
+			accordions.forEach((accordion) => closeAccordion(accordion));
+			openAccordion(accordion);
+			}
+		};
+	});
 });
